4975f0fe4eee9f60b7ca80c66653449545607e04
Abdullah Sajjad <abdullahsa026@gmail.com>
2022-12-06 20:00:27 +0500

    Fix: added a middleware to handle cors errors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
-const cors = require("cors");
+// const cors = require("cors");
 
 const {errorHandler} = require("./middlewares");
 const {goalRoutes, userRoutes} = require("./routes");
@@ -10,13 +10,22 @@ const {connectToDB} = require("./configs");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-let corsOptions = {
-  origin: process.env.FRONTEND_URL,
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+// const corsOptions = {
+//   origin: process.env.FRONTEND_URL,
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
 
 // some middlewares
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
