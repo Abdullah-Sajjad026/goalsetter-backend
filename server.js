8062a176ce7c2188f8cca947336eea82b2d28904
Abdullah Sajjad <abdullahsa026@gmail.com>
2022-12-06 21:36:34 +0500

    Fix: allowed access for everyone with CORS package

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
-// const cors = require("cors");
+const cors = require("cors");
 
 const {errorHandler} = require("./middlewares");
 const {goalRoutes, userRoutes} = require("./routes");
@@ -16,16 +16,16 @@ const app = express();
 // };
 
 // some middlewares
-// app.use(cors(corsOptions));
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  next();
-});
+app.use(cors());
+// app.use( (req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   next();
+// });
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
